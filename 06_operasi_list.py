f89f83f7d14df466e813076c737a56c0c3a2b06b
Rahman-Zebua <rachzebua@gmail.com>
2026-09-25 18:09:53 +0700

    Add files via upload

---
+++ 06_operasi_list.py
@@ -0,0 +1,11 @@
+# Contoh 1.6 - Operasi dasar pada list
+data = [10, 20, 30]
+
+print(data[1])  # Mengakses indeks 1
+data.append(40)  # Menambahkan data di akhir
+data.insert(0, 5)  # Menyisipkan data di awal
+
+print(data)
+
+data.pop()  # Menghapus elemen terakhir
+print(data)
---
+++ 07_pencarian_mahasiswa.py
@@ -0,0 +1,27 @@
+# Contoh 1.7 dan 1.8 - Pencarian data mahasiswa
+
+# Pencarian menggunakan list
+mahasiswa_list = [
+    ("22001", "Alya"),
+    ("22002", "Bima"),
+    ("22003", "Citra")
+]
+
+
+def cari_nim(data, nim):
+    for item in data:
+        if item[0] == nim:
+            return item
+    return None
+
+
+print(cari_nim(mahasiswa_list, "22003"))
+
+# Pencarian menggunakan dictionary
+mahasiswa_dict = {
+    "22001": "Alya",
+    "22002": "Bima",
+    "22003": "Citra"
+}
+
+print(mahasiswa_dict.get("22003"))
